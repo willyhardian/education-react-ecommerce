1340eed959687118afbd2b7f83739dd6a10634bc
Willy Hardian <hardian.will@gmail.com>
2024-07-12 17:59:09 +0700

    add product context in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { jwtDecode } from "jwt-decode";
 import Login from "./pages/Login";
 import Seller from "./pages/Seller";
 import Logout from "./pages/Logout";
+import { ProductContext } from "./store/fetch-product-context";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const router = createBrowserRouter([
     {
         path: "/login/seller",
@@ -73,7 +76,31 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        async function fetchProducts() {
+            const response = await axios.get(
+                import.meta.env.VITE_BASE_URL + "/products?category=2",
+                {
+                    headers: {
+                        Authorization:
+                            "Bearer " + localStorage.getItem("access_token"),
+                    },
+                }
+            );
+            console.log(response, "< response");
+            setProducts(response.data.rows);
+            const products = response.data.rows;
+            console.log(products, "< products");
+        }
+        fetchProducts();
+    }, []);
+
+    return (
+        <ProductContext.Provider value={{ products, setProducts }}>
+            <RouterProvider router={router} />;
+        </ProductContext.Provider>
+    );
 }
 
 export default App;
--- src/pages/Seller.jsx
+++ src/pages/Seller.jsx
@@ -1,30 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import { ProductContext } from "../store/fetch-product-context";
 export default function Seller() {
-    useEffect(() => {
-        async function fetchProducts() {
-            const response = await axios.get(
-                import.meta.env.VITE_BASE_URL + "/products?category=2",
-                {
-                    headers: {
-                        Authorization:
-                            "Bearer " + localStorage.getItem("access_token"),
-                    },
-                }
-            );
-            console.log(response, "< response");
-            const products = response.data.rows;
-            console.log(products, "< products");
-        }
-        fetchProducts();
-    }, []);
-
+    const { products, setProducts } = useContext(ProductContext);
     return (
         <>
             <Navbar />
             <div className="container mx-auto">
-                <h1 className="font-bold text-3xl">Seller</h1>
+                <div className="bg-white">
+                    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                            Seller
+                        </h2>
+                        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                            {products &&
+                                products.map((product) => {
+                                    return (
+                                        <div className="group relative">
+                                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                                <img
+                                                    src={product.imgUrl}
+                                                    alt="Front of men&#039;s Basic Tee in black."
+                                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                                />
+                                            </div>
+                                            <div className="mt-4 flex justify-between">
+                                                <div>
+                                                    <h3 className="text-sm text-gray-700">
+                                                        <a href="#">
+                                                            <span
+                                                                aria-hidden="true"
+                                                                className="absolute inset-0"
+                                                            ></span>
+                                                            {product.title}
+                                                        </a>
+                                                    </h3>
+                                                    {/* <p className="mt-1 text-sm text-gray-500">
+                                                    Black
+                                                </p> */}
+                                                </div>
+                                                <p className="text-sm font-medium text-gray-900">
+                                                    {product.price}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
